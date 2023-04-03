4cd18a4897614d2c623ec6f0debd09a9a13552f5
Jennifer Alexander <jenny_alexander@icloud.com>
2023-04-03 14:34:49 -0500

    Static mockup of highlights component

--- src/components/Highlights/Highlights.tsx
+++ src/components/Highlights/Highlights.tsx
@@ -3,9 +3,30 @@ import styles from './Highlights.module.scss';
 //import cx from 'classnames';
 
 const Highlights: React.FC = () => {
+
+    const sections: string[] = ['temperature',
+                                 'wind',
+                                 'rain',
+                                 'humidity',
+                                 'air quality',
+                                 'uv',
+                                ]
+
     return (
         <div className={styles.highlights}>
             <div className={styles.highlightsTitle}>Today's Highlights</div>
+            <div className={styles.highlightsDetails}>
+            {sections.map(section => {
+                    return (
+                        <div className={styles.sectionDetails}>
+                            <div className={styles.sectionTitle}>{section}</div>
+                            <div className={styles.sectionInfo}>
+                                <img className={styles.image} src="/images/humidity.png"/>
+                                <div className={styles.sectionReading}>Average</div>
+                            </div>
+                        </div>)
+                })}
+            </div>
         </div>
     )
 }
