0d120065b7cef778af850f1cd4c9422b519a19c0
Jennifer Alexander <jenny_alexander@icloud.com>
2023-04-03 15:05:00 -0500

    Finish desktop mockup

--- src/components/Highlights/Highlights.tsx
+++ src/components/Highlights/Highlights.tsx
@@ -4,25 +4,56 @@ import styles from './Highlights.module.scss';
 
 const Highlights: React.FC = () => {
 
-    const sections: string[] = ['temperature',
-                                 'wind',
-                                 'rain',
-                                 'humidity',
-                                 'air quality',
-                                 'uv',
+    interface IHighlights {
+        section: string;
+        image: string;
+        reading: string;
+    }
+
+    const highlights: IHighlights[] = [
+        {
+            section: 'temperature',
+            image: '/images/thermometer.png',
+            reading: '40°F | 33°F',
+        },
+        {
+            section: 'wind',
+            image: '/images/wind.png',
+            reading: '10.3 mi/h',
+        },
+        {
+            section: 'rain',
+            image: '/images/rain.png',
+            reading: '45%',
+        },
+        {
+            section: 'humidity',
+            image: '/images/humidity.png',
+            reading: '70%',
+        },
+        {
+            section: 'air quality',
+            image: '/images/oxygen.png',
+            reading: 'Good',
+        },
+        {
+            section: 'uv',
+            image: '/images/rays.png',
+            reading: 'High',
+        },
                                 ]
 
     return (
         <div className={styles.highlights}>
             <div className={styles.highlightsTitle}>Today's Highlights</div>
             <div className={styles.highlightsDetails}>
-            {sections.map(section => {
+            {highlights.map(highlights => {
                     return (
                         <div className={styles.sectionDetails}>
-                            <div className={styles.sectionTitle}>{section}</div>
+                            <div className={styles.sectionTitle}>{highlights.section}</div>
                             <div className={styles.sectionInfo}>
-                                <img className={styles.image} src="/images/humidity.png"/>
-                                <div className={styles.sectionReading}>Average</div>
+                                <img className={styles.image} src={highlights.image}/>
+                                <div className={styles.sectionReading}>{highlights.reading}</div>
                             </div>
                         </div>)
                 })}
