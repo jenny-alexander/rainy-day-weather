1a18e09ad70809122f11552b19c26a947cd89d17
Jennifer Alexander <jenny_alexander@icloud.com>
2023-04-27 14:16:32 -0500

    Check that font is loaded before displaying app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,7 +6,6 @@ import WeeklyForecast from '../Forecast/Weekly/WeeklyForecast';
 import Highlights from '../Highlights/Highlights';
 import Switch from '../Common/Switch/Switch';
 import { IWeatherResponseDTO } from '../../api/weather/weatherApi';
-import cx from 'classnames';
 
 const App = (): JSX.Element => {
   const [mobileView, setMobileView] = useState(false);
@@ -16,10 +15,17 @@ const App = (): JSX.Element => {
   const [alert, setAlert] = useState<boolean>(false);
   const [isToggled, setIsToggled] = useState<boolean>(true);
   const [theme, setTheme] = useState<string>('light');
+  const [fontLoaded, setFontLoaded] = useState<boolean>(false);
 
   useEffect(() => {
       setMobileView(mql.matches)
   },[mql.matches]);
+
+  useEffect(() => {
+    document.fonts.ready.then(() => {
+      setFontLoaded(true);
+    })
+  })
 
   mql.addEventListener('change', (e) => { setMobileView(mql.matches)  });
 
@@ -39,48 +45,53 @@ const App = (): JSX.Element => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
     setTheme(newTheme);
   }
+  if (!fontLoaded) {
+    return(
+      <></>
+    )
+  } else {
+      return (
+        <div className={styles.mainContainer} data-theme={theme}>
+          <div className={styles.menuOptions}>
+            <Switch isToggled={isToggled} onToggle={toggleMode} />
+            { alert ?  
+              <button className={styles.alerts}>
+                <i className="fa-solid fa-triangle-exclamation"/>
+              </button> 
+              : null 
+            }
+          </div>
+          { weather === undefined ? <div className={styles.centerAppContainer}></div> : null }
+          <div className={`${mobileView ? styles.smallAppContainer : styles.appContainer}`}>
 
-  return (
-    <div className={styles.mainContainer} data-theme={theme}>
-      <div className={styles.menuOptions}>
-        <Switch isToggled={isToggled} onToggle={toggleMode} />
-        { alert ?  
-          <button className={styles.alerts}>
-            <i className="fa-solid fa-triangle-exclamation"/>
-          </button> 
-          : null 
-        }
-      </div>
-      { weather === undefined ? <div className={styles.centerAppContainer}></div> : null }
-      <div className={`${mobileView ? styles.smallAppContainer : styles.appContainer}`}>
-
-        <SearchBar 
-            returnWeather={handleGetWeather}
-            returnLocation={handleGetLocation}
-          />
-        <div className={styles.titleContainer}>
-          <div className={styles.appTitle}>Rainy Day Weather</div>
+            <SearchBar 
+                returnWeather={handleGetWeather}
+                returnLocation={handleGetLocation}
+              />
+            <div className={styles.titleContainer}>
+              <div className={styles.appTitle}>Rainy Day Weather</div>
+            </div>
+              {
+                weather !== undefined ?
+                  <div className={styles.detailsContainer}>
+                    <DailyForecast 
+                      mobileView={mobileView}  
+                      weatherProp={weather}
+                      locationProp={location}     
+                    /> 
+                    <Highlights mobileView={mobileView} 
+                                weatherProp={weather}
+                    />
+                    <WeeklyForecast weatherProp={weather}/> 
+                  </div> : 
+                  <div className={styles.logoContainer}>
+                    <img className={styles.logo} src='/images/umbrella_yellow.png'></img>
+                  </div>
+              }
+          </div>
         </div>
-          {
-            weather !== undefined ?
-              <div className={styles.detailsContainer}>
-                <DailyForecast 
-                  mobileView={mobileView}  
-                  weatherProp={weather}
-                  locationProp={location}     
-                /> 
-                <Highlights mobileView={mobileView} 
-                            weatherProp={weather}
-                />
-                <WeeklyForecast weatherProp={weather}/> 
-              </div> : 
-              <div className={styles.logoContainer}>
-                <img className={styles.logo} src='/images/umbrella_yellow.png'></img>
-              </div>
-          }
-      </div>
-    </div>
-  );
+      );
+    }
 
 }
 
